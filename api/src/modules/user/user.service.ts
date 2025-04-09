113ae2c3c3cd17b3748a5146c3b43c77a74bf620
TioElvis <elvis.veramijares@gmail.com>
2025-04-09 19:35:11 +0200

    Add FindById method in UserService

--- api/src/modules/user/user.service.ts
+++ api/src/modules/user/user.service.ts
@@ -1,9 +1,23 @@
-import type { Model } from "mongoose";
-import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { User } from "../../schemas/user.schema";
+import { HttpException, Injectable } from "@nestjs/common";
+import { Types, type Model, type ProjectionType } from "mongoose";
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private _UserModel_: Model<User>) {}
+
+  async FindById(id: Types.ObjectId, projection?: ProjectionType<User>) {
+    if (Types.ObjectId.isValid(id) === false) {
+      throw new HttpException("Invalid id", 400);
+    }
+
+    const user = await this._UserModel_.findById(id, projection);
+
+    if (user === null) {
+      throw new HttpException("User not found", 404);
+    }
+
+    return user;
+  }
 }
