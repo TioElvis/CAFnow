300d318e2765e49dd86c40422a6c9b7dd10cb463
TioElvis <elvis.veramijares@gmail.com>
2025-04-10 18:26:47 +0200

    Add JWT handling and middleware for refresh_token validation

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,8 +1,11 @@
-import { API_URL } from "./lib/constants";
+import { jwtVerify } from "jose";
 import { NextResponse } from "next/server";
-import type { MiddlewareConfig } from "next/server";
+import { API_URL, JWT_SECRET } from "./lib/constants";
+import type { MiddlewareConfig, NextRequest } from "next/server";
 
-export async function middleware() {
+const secret = new TextEncoder().encode(JWT_SECRET);
+
+export async function middleware(request: NextRequest) {
   try {
     await fetch(new URL("/", API_URL));
   } catch (error) {
@@ -10,6 +13,33 @@ export async function middleware() {
       { error, text: "API is not responding" },
       { status: 500 },
     );
+  }
+
+  const { pathname } = request.nextUrl;
+  const refresh_token = request.cookies.get("refresh_token")?.value;
+
+  const isPrivate = pathname.startsWith("/private");
+
+  if (refresh_token === undefined && isPrivate === true) {
+    return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+  }
+
+  if (refresh_token !== undefined && isPrivate === true) {
+    try {
+      await jwtVerify(refresh_token, secret);
+    } catch (error) {
+      console.error(error);
+      return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+    }
+  }
+
+  if (refresh_token !== undefined && pathname === "/auth/sign-in") {
+    try {
+      await jwtVerify(refresh_token, secret);
+      return NextResponse.redirect(new URL("/private/my-profile", request.url));
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return NextResponse.next();
