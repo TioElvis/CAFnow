858756a8e7b80243323659ecf6e67e0d84b556f7
TioElvis <elvis.veramijares@gmail.com>
2025-04-10 17:48:39 +0200

    Fix middleware to use correct API_URL constant for fetch request

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,16 +1,18 @@
+import { API_URL } from "./lib/constants";
 import { NextResponse } from "next/server";
-import { BASE_API_URL } from "./lib/constants";
 import type { MiddlewareConfig } from "next/server";
 
 export async function middleware() {
   try {
-    await fetch(new URL("/", BASE_API_URL));
+    await fetch(new URL("/", API_URL));
   } catch (error) {
     return NextResponse.json(
       { error, text: "API is not responding" },
       { status: 500 },
     );
   }
+
+  return NextResponse.next();
 }
 
 export const config: MiddlewareConfig = {
