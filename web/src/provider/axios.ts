4b755169904d6589ce532ee5dbcb9f7079ce1d10
TioElvis <elvis.veramijares@gmail.com>
2025-04-10 17:46:51 +0200

    Update API URL constant

--- web/src/provider/axios.ts
+++ web/src/provider/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { BASE_API_URL } from "@/lib/constants";
+import { API_URL } from "@/lib/constants";
 
 export const _axios = axios.create({
-  baseURL: BASE_API_URL,
+  baseURL: API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
