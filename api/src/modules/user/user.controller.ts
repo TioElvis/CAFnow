516ee5828b0e400c3bbf81b858cc80468da520bd
TioElvis <elvis.veramijares@gmail.com>
2025-04-14 17:57:07 +0200

    Add protection to user routes; Add user find by id endpoint

--- api/src/modules/user/user.controller.ts
+++ api/src/modules/user/user.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from "@nestjs/common";
+import { Types } from "mongoose";
 import { UserService } from "./user.service";
+import { AuthGuard } from "@nestjs/passport";
+import { Controller, Get, Param, UseGuards } from "@nestjs/common";
 
 @Controller("user")
+@UseGuards(AuthGuard("jwt"))
 export class UserController {
   constructor(private _UserService_: UserService) {}
+
+  @Get("find-by-id/:id")
+  async FindById(@Param("id") id: Types.ObjectId) {
+    return await this._UserService_.FindById(id, {
+      password: false,
+      finger_print: false,
+    });
+  }
 }
