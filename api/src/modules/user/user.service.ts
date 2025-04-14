6ff9dc9018ae41c6127e8a37e5f8c0d96cc2f90b
TioElvis <elvis.veramijares@gmail.com>
2025-04-14 17:54:56 +0200

    Create find user by id method

--- api/src/modules/user/user.service.ts
+++ api/src/modules/user/user.service.ts
@@ -1,9 +1,24 @@
-import type { Model } from "mongoose";
-import { Injectable } from "@nestjs/common";
+import { Types } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
 import { User } from "../../schemas/user.schema";
+import type { Model, ProjectionType } from "mongoose";
+import { HttpException, Injectable } from "@nestjs/common";
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private _UserModel_: Model<User>) {}
+
+  async FindById(id: Types.ObjectId, projection?: ProjectionType<User>) {
+    if (Types.ObjectId.isValid(id) === false) {
+      throw new HttpException("id non valida", 400);
+    }
+
+    const user = await this._UserModel_.findById(id, projection);
+
+    if (user === null) {
+      throw new HttpException("Utente non trovato", 404);
+    }
+
+    return user;
+  }
 }
