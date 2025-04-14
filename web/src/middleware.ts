279ec4f70f8472c48c27a1a08ecf26beac87256b
TioElvis <elvis.veramijares@gmail.com>
2025-04-14 20:42:36 +0200

    Protect private routes and verify if the refresh_token is valid

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,8 +1,10 @@
 import { NextResponse } from "next/server";
 import { _axios } from "./providers/axios/ssr";
-import { type MiddlewareConfig } from "next/server";
+import type { MiddlewareConfig, NextRequest } from "next/server";
 
-export async function middleware() {
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
   try {
     await _axios.get("/");
   } catch (error) {
@@ -11,6 +13,30 @@ export async function middleware() {
       { text: "API is not responding" },
       { status: 500 },
     );
+  }
+  const isPrivate = pathname.startsWith("/private");
+  const refresh_token = request.cookies.get("refresh_token")?.value;
+
+  if (refresh_token === undefined && isPrivate === true) {
+    return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+  }
+
+  if (refresh_token !== undefined) {
+    const response = await _axios.get("/auth/check-refresh-token-integrity", {
+      headers: {
+        Cookie: `refresh_token=${refresh_token}`,
+      },
+    });
+
+    const ok = response.status === 200;
+
+    if (isPrivate === true && ok === false) {
+      return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+    }
+
+    if (pathname === "/auth/sign-in" && ok === true) {
+      return NextResponse.redirect(new URL("/private", request.url));
+    }
   }
 
   return NextResponse.next();
