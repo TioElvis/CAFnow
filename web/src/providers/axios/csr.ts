c34f1e70854a3b8539703d855fdead9688633aba
TioElvis <elvis.veramijares@gmail.com>
2025-04-17 20:07:43 +0200

    Add response interceptor to handle 401 errors and refresh access_token

--- web/src/providers/axios/csr.ts
+++ web/src/providers/axios/csr.ts
@@ -7,3 +7,34 @@ export const _axios = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+let isRefreshing = false;
+
+_axios.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const request = error.config;
+    const status = error.response?.status;
+
+    if (status === 401 && !request._retry) {
+      request._retry = true;
+
+      if (isRefreshing === false) {
+        isRefreshing = true;
+
+        try {
+          await _axios.get("/auth/refresh-access-token");
+
+          return _axios(request);
+        } catch (error) {
+          window.location.href = "/auth/sign-in";
+          return Promise.reject(error);
+        } finally {
+          isRefreshing = false;
+        }
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
