1313ade46b3cc66af0b572d329a580523612c7b1
TioElvis <elvis.veramijares@gmail.com>
2025-04-19 15:32:39 +0200

    Implement AdminController with find-all endpoint for admin users

--- api/src/modules/user/admin/admin.controller.ts
+++ api/src/modules/user/admin/admin.controller.ts
@@ -1,8 +1,20 @@
 import { AuthGuard } from "@nestjs/passport";
-import { Controller, UseGuards } from "@nestjs/common";
+import { UserService } from "../user.service";
+import { UserRole } from "../../../schemas/user.schema";
+import { Controller, Get, UseGuards } from "@nestjs/common";
 import { Roles, RolesGuard } from "../../../guards/roles.guard";
 
 @Controller("admin")
 @UseGuards(AuthGuard("jwt"), RolesGuard)
 @Roles("admin")
-export class AdminController {}
+export class AdminController {
+  constructor(private _UserService_: UserService) {}
+
+  @Get("find-all")
+  async FindAll() {
+    return await this._UserService_.FindAll(
+      { role: UserRole.ADMIN },
+      { password: false, finger_print: false },
+    );
+  }
+}
