38ef607be3ce0bb160ecb2b9e63b91a258ffda3b
TioElvis <elvis.veramijares@gmail.com>
2025-04-20 10:39:09 +0200

    Add ResendProvider for email handling and template compilation

--- api/src/lib/constants.ts
+++ api/src/lib/constants.ts
@@ -1,3 +1,7 @@
+import { join } from "node:path";
+
 export const TEMPORARY_TOKEN_EXPIRES = 5 * 60;
 export const ACCESS_TOKEN_EXPIRES = 1 * 60 * 60;
 export const REFRESH_TOKEN_EXPIRES = 7 * 24 * 60 * 60;
+
+export const TEMPLATES_ROOT = join(__dirname, "assets");
