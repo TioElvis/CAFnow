e72f5f9ec2c2c94c8871e4650f5b0ab3cddd85ef
TioElvis <elvis.veramijares@gmail.com>
2025-04-26 19:24:27 +0200

    Add page structure with managers table

--- web/src/app/(root)/private/(routes)/manager/page.tsx
+++ web/src/app/(root)/private/(routes)/manager/page.tsx
@@ -1,5 +1,36 @@
-import { Fragment } from "react";
+import Link from "next/link";
+import { getAllManagers } from "./actions";
+import { columns } from "./components/columns";
+import { Button } from "@/components/ui/button";
+import { TableProvider } from "@/context/table";
+import { DataTable } from "@/components/data-table";
+import { DataTablePagination } from "@/components/data-table-pagination";
+import { DataTableGlobalFilter } from "@/components/data-table-global-filter";
+import { DataTableDeleteRowsSelected } from "@/components/data-table-delete-rows-selected";
 
-export default function Page() {
-  return <Fragment></Fragment>;
+export default async function Page() {
+  /*
+   For the future: Check if the user is a admin or a super show all managers else show only the managers that are connected with the same CAF
+  */
+  const managers = await getAllManagers();
+
+  return (
+    <div className="container mx-auto py-10">
+      <TableProvider data={managers} columns={columns}>
+        <section className="space-y-4">
+          <div className="w-full flex flex-col items-center justify-between gap-4 lg:flex-row">
+            <DataTableGlobalFilter className="w-full lg:w-96" />
+            <div className="w-full flex gap-2 lg:w-auto">
+              <DataTableDeleteRowsSelected url="/manager/delete-many" />
+              <Link href="/private/manager/create" className="flex-grow">
+                <Button className="w-full">Crea un manager</Button>
+              </Link>
+            </div>
+          </div>
+          <DataTable />
+          <DataTablePagination />
+        </section>
+      </TableProvider>
+    </div>
+  );
 }
