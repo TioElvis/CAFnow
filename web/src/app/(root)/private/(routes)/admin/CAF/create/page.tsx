2f881e92f9323113410b40d524dbcedd9df5c98f
TioElvis <elvis.veramijares@gmail.com>
2025-04-26 22:18:35 +0200

    Remove error handling from super managers retrieval in CAF creation page

--- web/src/app/(root)/private/(routes)/admin/CAF/create/page.tsx
+++ web/src/app/(root)/private/(routes)/admin/CAF/create/page.tsx
@@ -2,12 +2,7 @@ import { CreateCAFForm } from "./form";
 import { getAllSuperManagers } from "../../../manager/actions";
 
 export default async function Page() {
-  try {
-    const super_managers = await getAllSuperManagers();
+  const super_managers = await getAllSuperManagers();
 
-    return <CreateCAFForm super_managers={super_managers} />;
-  } catch (error) {
-    console.error(error);
-    return <>Error in Super managers page</>;
-  }
+  return <CreateCAFForm super_managers={super_managers} />;
 }
